2a8fff73134087008fc595f689bf04a3a18a5a10
Bruce Schuchardt <bschuchardt@pivotal.io>
2019-05-09 08:32:31 -0700

    linux changes

--- opensh/stresstest.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-exec 2>&1
-exec >stresstest.log
-for x in "$@"; do
-  stressTest="$stressTest --test $x"
-done
-gr repeatDistributedTest $stressTest \
-  --no-parallel -Prepeat=50 -PfailOnNoMatchingTests=false
